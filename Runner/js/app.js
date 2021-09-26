7a78e0ef682a088ecb22c3d615dc1c018bdb107f
Fuad Hasan <fuad.cs22@outlook.com>
2021-09-26 19:31:40 +0600

    v

--- Runner/js/app.js
+++ Runner/js/app.js
@@ -28,7 +28,7 @@ function media_f(){
     let media = window.matchMedia("(orientation: portrait)")
     if (media.matches){
         speed *= 2;
-        gravity *= 3;
+        gravity *= 2.5;
         maxGravity = gravity;
         upforce *= 1.5;
         console.log("Media");
