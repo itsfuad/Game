ac7889fde55dee1b4faa71b1ff83af7392c579cb
itsfuad <fuad.cs22@gmail.com>
2021-10-18 01:32:01 +0600

    uff

--- Tictactoe/script.js
+++ Tictactoe/script.js
@@ -5,8 +5,8 @@ const text = document.getElementsByClassName('text')[0];
 const close = document.getElementsByClassName('close')[0];
 const hint = document.getElementsByClassName('hint')[0];
 let button = Math.round(Math.random());
-let turn = button ? '×' : '○';
-let pc_turn = button ? '○' : '×';
+let turn = button ? '😘' : '💙';
+let pc_turn = button ? '💙' : '😘';
 let played_tiles = 0;
 //alert(`${turn} is your symbol`);
 
@@ -20,7 +20,7 @@ if (!button){
 
 function who(state){
     if(state == 1){
-        if(turn == '○'){
+        if(turn == '💙'){
             text.innerText = "You won";
         }else{
             text.innerText = "Computer won";
@@ -28,7 +28,7 @@ function who(state){
         message.classList.add('active');
         return;
     }else if(state == 2){
-        if (turn == '×') {
+        if (turn == '😘') {
             text.innerText = "You won";
         } else {
             text.innerText = "Computer won";
@@ -49,18 +49,17 @@ function sleep(ms) {
 }
 
 cells.forEach(cell => {
-    cell.addEventListener('click', async () =>{
-        if(cell.innerText != '○' && cell.innerText != '×'){
+    cell.addEventListener('click', () =>{
+        if(cell.innerText != '💙' && cell.innerText != '😘'){
             cell.style.color = button ? '#ff6969' : 'skyblue';
             cell.innerText = turn;
             played_tiles++;
-            await win();
-            await sleep(100);
-            await computer();
+            who(win());
+            computer();
           //  console.log('Already Occupied!');
         }
         //button = Math.round(Math.random());
-        //turn = button ? '×' : '○';
+        //turn = button ? '😘' : '💙';
 
     });
 });
@@ -69,52 +68,54 @@ cells.forEach(cell => {
 function computer(){
     let pc_pos = Math.floor(Math.random()*8);
    // console.log(pc_pos);
-   if (played_tiles >= 9) {
-       who(3);
-       return;
-   };
+   
    console.log(played_tiles);
-    if(cells[pc_pos].innerText != '○' && cells[pc_pos].innerText != '×'){
+   if(played_tiles >= 9){
+     who(win()?0:3);
+   }else{
+    if(cells[pc_pos].innerText != '💙' && cells[pc_pos].innerText != '😘'){
+        
       //  console.log('^');
         cells[pc_pos].innerText = pc_turn;
         cells[pc_pos].style.color = !button ? '#ff6969':'skyblue';
         played_tiles++;
-        win();
+        who(win());
+        
     }
     
     else{
         computer();
     }
+   }
 }
 
 function win(){
     if (
-        (cells[0].innerText == '○' && cells[1].innerText == '○' && cells[2].innerText == '○') 
-        || (cells[3].innerText == '○' && cells[4].innerText == '○' && cells[5].innerText == '○')
-        || (cells[6].innerText == '○' && cells[7].innerText == '○' && cells[8].innerText == '○')
-        || (cells[0].innerText == '○' && cells[3].innerText == '○' && cells[6].innerText == '○')
-        || (cells[1].innerText == '○' && cells[4].innerText == '○' && cells[7].innerText == '○')
-        || (cells[2].innerText == '○' && cells[5].innerText == '○' && cells[8].innerText == '○')
-        || (cells[0].innerText == '○' && cells[4].innerText == '○' && cells[8].innerText == '○')
-        || (cells[2].innerText == '○' && cells[4].innerText == '○' && cells[6].innerText == '○')
+        (cells[0].innerText == '💙' && cells[1].innerText == '💙' && cells[2].innerText == '💙') 
+        || (cells[3].innerText == '💙' && cells[4].innerText == '💙' && cells[5].innerText == '💙')
+        || (cells[6].innerText == '💙' && cells[7].innerText == '💙' && cells[8].innerText == '💙')
+        || (cells[0].innerText == '💙' && cells[3].innerText == '💙' && cells[6].innerText == '💙')
+        || (cells[1].innerText == '💙' && cells[4].innerText == '💙' && cells[7].innerText == '💙')
+        || (cells[2].innerText == '💙' && cells[5].innerText == '💙' && cells[8].innerText == '💙')
+        || (cells[0].innerText == '💙' && cells[4].innerText == '💙' && cells[8].innerText == '💙')
+        || (cells[2].innerText == '💙' && cells[4].innerText == '💙' && cells[6].innerText == '💙')
     ){
-        who(1);
-        return;
+        return 1;
     }
     else if (
-        (cells[0].innerText == '×' && cells[1].innerText == '×' && cells[2].innerText == '×') 
-        || (cells[3].innerText == '×' && cells[4].innerText == '×' && cells[5].innerText == '×')
-        || (cells[6].innerText == '×' && cells[7].innerText == '×' && cells[8].innerText == '×')
-        || (cells[0].innerText == '×' && cells[3].innerText == '×' && cells[6].innerText == '×')
-        || (cells[1].innerText == '×' && cells[4].innerText == '×' && cells[7].innerText == '×')
-        || (cells[2].innerText == '×' && cells[5].innerText == '×' && cells[8].innerText == '×')
-        || (cells[0].innerText == '×' && cells[4].innerText == '×' && cells[8].innerText == '×')
-        || (cells[2].innerText == '×' && cells[4].innerText == '×' && cells[6].innerText == '×')
+        (cells[0].innerText == '😘' && cells[1].innerText == '😘' && cells[2].innerText == '😘') 
+        || (cells[3].innerText == '😘' && cells[4].innerText == '😘' && cells[5].innerText == '😘')
+        || (cells[6].innerText == '😘' && cells[7].innerText == '😘' && cells[8].innerText == '😘')
+        || (cells[0].innerText == '😘' && cells[3].innerText == '😘' && cells[6].innerText == '😘')
+        || (cells[1].innerText == '😘' && cells[4].innerText == '😘' && cells[7].innerText == '😘')
+        || (cells[2].innerText == '😘' && cells[5].innerText == '😘' && cells[8].innerText == '😘')
+        || (cells[0].innerText == '😘' && cells[4].innerText == '😘' && cells[8].innerText == '😘')
+        || (cells[2].innerText == '😘' && cells[4].innerText == '😘' && cells[6].innerText == '😘')
     ){
-        who(2);
-        return;
+ 
+        return 2;
     }
-    return;
+    return 0;
 }
 
 function reset(){
@@ -123,8 +124,8 @@ function reset(){
     });
     played_tiles = 0;
     button = Math.round(Math.random());
-    turn = button ? '×' : '○';
-    pc_turn = button ? '○' : '×';
+    turn = button ? '😘' : '💙';
+    pc_turn = button ? '💙' : '😘';
     hint.innerText = `Your Symbol is ${turn}`;
     message.classList.remove('active');
 }
