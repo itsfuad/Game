f8dfcb587166920c2125ae0ac07eff406d940293
itsfuad <fuad.cs22@gmail.com>
2021-10-22 10:57:57 +0600

    😑

--- Hit/src/js/main.js
+++ Hit/src/js/main.js
@@ -1,62 +1,89 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
-canvas.height = canvas.width = 400;
+canvas.height = window.innerHeight;
+canvas.width = window.innerWidth;
+
 ctx.font = '16px Arial';
 
-const gs = canvas.height/3;
-const gc = canvas.height/6;
-let tile = [];
+let x = 0, y = 0;
+let score = 0, highscore = localStorage.getItem('hit_hsc')||0;
+let timeT = 60;
+let over = false;
+let free = true;
 const doge = new Image();
-doge.src = 'src/images/takla7.png';
+doge.src = 'src/images/doge.png';
 
-const drawGrid = () => {
-    for( let i = gs; i < canvas.height; i+=gs){
-        console.log(`Grid cordinates: ${i}`);
-        ctx.beginPath();
-        ctx.moveTo(i, 0);
-        ctx.lineTo(i, canvas.height);
-        ctx.moveTo(0, i);
-        ctx.lineTo(canvas.width, i);
-        ctx.stroke();
-        //ctx.closePath()
-    }
-}
 
-const drawHoles = () => {
-    for( let i = gc; i < canvas.height; i+=gc*2){
-        for (let j = gc; j < canvas.height; j+=gc*2){
-            //console.log(`Center cordinates: ${i}, ${j}`);
-            ctx.beginPath();
-            //ctx.arc(i, j, 20, 0, 2*Math.PI);
-            ctx.ellipse(i, j, 20, 10, 0, 0, Math.PI*2);
-            ctx.fill();
-        }
-    }
-}
-
-const getTileCoordinates = () => {
-    for( let i = gc; i < canvas.height; i+=gc*2){
-        for (let j = gc; j < canvas.height; j+=gc*2){
-            //console.log(`Center cordinates: ${i}, ${j}`);
-            //ctx.beginPath();
-            //ctx.arc(i, j, 20, 0, 2*Math.PI);
-            //ctx.fill();
-            tile.push({x: i, y: j});
-        }
-    }
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 const drawDoge = () => {
+    x = Math.floor(Math.random()*(canvas.width - 55));
+    y = Math.floor(Math.random()*(canvas.height - 47));
+    ctx.drawImage(doge, 0, 0, 55, 47, x, y, 55, 47);
     
-    //ctx.drawImage(doge, 0, 0, 55, 55, tile[0].x, tile[0].y, canvas.width, canvas.height);
-    ctx.drawImage(doge, 55, 142);
+    
+   // ctx.drawImage(doge, 0, 0);
 }
 
-drawGrid();
-drawHoles();
-getTileCoordinates();
-ctx.fillStyle = 'white';
-ctx.fillText("Hello", tile[0].x, tile[0].y);
+canvas.addEventListener('touchstart', async (evt) => {
+    let tx = getTouches(evt)[0].clientX;
+    let ty = getTouches(evt)[0].clientY;
+    
+    if ((tx >= x && tx <= x + 55)
+    && (ty >= y && ty <= y + 47)){
+  //  ctx.clearRect(0,0,canvas.width, canvas.height);
+      if (free){
+      ctx.drawImage(doge, 0, 88, 55, 47, x, y, 55, 47);
+      score++;
+      free = false;
+      await sleep(800);
+      free = true;
+      }
+    
+    }
+});
+function getTouches(evt) {
+    return evt.touches || // browser API
+        evt.originalEvent.touches; // jQuery
+}
+
+async function timer(){
+    if(timeT <= 0){
+        gameover();
+    }
+    else{
+        timeT--;
+        await sleep(1000);
+    }
+    //requestAnimationFrame(timer);
+    timer();
+}
+function gameover(){
+    if (score >= highscore){
+        localStorage.setItem("hit_hsc", score);
+    }
+    over=true;
+}
+
 //drawDoge();
-ctx.drawImage(doge, canvas.width, canvas.height);
+const gameLoop = async () => {
+  if(!over){
+       
+    
+    ctx.clearRect(0,0,canvas.width, canvas.height);
+    ctx.fillStyle = 'black';
+    ctx.fillText(`Score: ${score}`, 10, 20);
+    ctx.fillText(`High Score: ${highscore}`, 10, 40);
+    ctx.fillText(`Time remaining: ${timeT} seconds`, canvas.width/2, 30);
+    drawDoge();
+    await sleep(500);
+    //timer();
+   requestAnimationFrame(gameLoop);
+  }
+    
+}
+timer();
+gameLoop();
